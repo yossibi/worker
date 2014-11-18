3817884c4d056e49bca8cc03bb30d24200b80244
yossibi <yosefbenishai@gmail.com>
2014-11-18 22:16:39 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 var i = 0;
 
 function boom() {
-    for(i=0; i!==-1;  i++){
-   if( i%7 == 0 )
+    for(i=0; i<22;  i++){
+   if( i%7 == 0 ){
     postMessage(i);
-    setTimeout("timedCount()",100);
-    }
+   // setTimeout("timedCount()",100);
+    }}
 }
 boom();
