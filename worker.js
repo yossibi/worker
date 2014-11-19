a3644c0fa2614a7ad555fa179cbe051f1b1e8d07
yossibi <yosefbenishai@gmail.com>
2014-11-19 20:43:14 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,24 @@
+
 function boom() {
     for(var i=0; i<22;  i++){
    if( i%7 == 0 ){
     postMessage(i);
-   // setTimeout("timedCount()",100);
+pausecomp(5000);
+ 
+ 
+ 
+ //  setTimeout("boom()",1000);
     }}
 }
 boom();
+
+
+ function pausecomp(millis)
+ {
+  var date = new Date();
+  var curDate = null;
+  do {
+	curDate = new Date(); 
+	}
+	while(curDate-date < millis);
+}
